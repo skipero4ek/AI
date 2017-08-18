3eb0d7d3b56ef52944b126162244d4d08525b68b
MadHunterrr <evgeniybylym@gmail.com>
2017-08-18 09:50:09 +0300

    hello

---
+++ AI/Hello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._08._2017CW
+{
+    class Hello
+    {
+        public static string Result()
+        {
+            return "Привет";
+        }
+        public static string Result(string data)
+        {
+            string[] Items = File.ReadAllLines("hello.txt");
+
+            return "";
+        }
+
+    }
+}
