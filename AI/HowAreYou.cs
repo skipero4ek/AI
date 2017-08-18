a2f584a635fe0cc6b3e984f8f7cd07d2f23cbd63
skipero4ek <toxt@mail.ru>
2017-08-18 09:30:59 +0300

    toxa

--- AI/HowAreYou.cs
+++ AI/HowAreYou.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Collections;
 
 namespace AI
 {
-    class HowAreYou
+     class HowAreYou
     {
+        HowAreYou() { }
+
+        public string Result()
+        { 
+        return Result("how");
+        }
+        public string Result(String str)
+        {
+            Random r = new Random();
+            string[]sArr= File.ReadAllLines("Answer.txt");
+            List<string> ls = new List<string>();
+                foreach(string x in sArr)
+                {
+                    if(x.IndexOf(str)!=-1)
+                    {
+                        ls.Add(x);
+                    }
+                }
+            int index = r.Next(ls.Count);
+            string res=ls[index].ToString();
+            return res;
+        }
+
     }
 }
