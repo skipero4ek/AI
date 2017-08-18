21e0c1d7c817d9b9a3df449db52650b29cda6587
ilonamor <ilonapl@president.org.ua>
2017-08-18 09:40:25 +0300

    Task

---
+++ AI/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AI
+{
+    class Task
+    {
+        public static string Result()
+        {
+            return "Result:";
+        }
+
+        public static string Result(string data)
+        {
+            string[] items = File.ReadAllLines("Task.txt",Encoding.UTF8);
+            List<string> good = new List<string>();
+            foreach(var temp in items)
+            {
+                if(temp.IndexOf(data)!=-1) // проверяем наличие элемента 
+                {
+                    good.Add(temp);
+                }
+            }
+           Random r = new Random();
+           return good[r.Next(0,good.Count)];
+        }
+    }
+}
